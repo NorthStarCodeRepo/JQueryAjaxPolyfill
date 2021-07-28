17095f86293af333b0cc322abaa53699f37e86f2
charles.jennings <charles.jennings@fun.com>
2021-07-28 15:33:02 -0500

    Added file and documentation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JQueryAjaxPolyfill
+
+Use this polyfill to get JQuery Ajax style async requests done more easily than writing out long XMLHTTPRequest objects.
+
+Simply call to the static global AJAXHelper instance and pass in a request object like you'd use in JQuery
+    - https://api.jquery.com/jquery.ajax/#jQuery-ajax-settings
---
+++ jquery-ajax-polyfill.js
@@ -0,0 +1,76 @@
+/**
+ * Helper class to be used like jQuery ajax method on old devices.
+ * */
+ var AJAXHelper = function () { };
+
+ /**
+  * A jQuery ajax style method for async requests.
+  * https://api.jquery.com/jquery.ajax/#jQuery-ajax-settings
+  * @param {contentType, method, data, URL} requestObject - jQuery-like ajax [settings] object.
+  * {data} must be of type: string, please serialize prior to passing to this method.
+  * @param {successCallback} successCallback - Handles a successful request.
+  * @param {failureCallback} failureCallback - Handles a failed request.
+  * @returns {void}
+  */
+  AJAXHelper.prototype.ajax = function (requestObject, successCallback, failureCallback)
+ {
+     var httpRequest;
+ 
+     if (window.XMLHttpRequest)
+     {
+         // Mozilla, Safari, IE7+ ...
+         httpRequest = new XMLHttpRequest();
+     }
+     else if (window.ActiveXObject)
+     {
+         // IE 6 and older
+         httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+     }
+ 
+     if (!httpRequest)
+     {
+         failureCallback('Oops, please try again.');
+         return false;
+     }
+ 
+     httpRequest.onreadystatechange = function ()
+     {
+         //Ready States => UNSENT: 0, OPENED: 1, HEADERS_RECEIVED: 2, LOADING: 3, DONE: 4
+         if (httpRequest.readyState == 4)
+         {
+             // Our response from the server. String data, typically.
+             var response = httpRequest.responseText;
+ 
+             if (httpRequest.status === 200)
+             {
+                 successCallback(response);
+             }
+             else if (httpRequest.status === 204) // No data returned, but successful.
+             {
+                 successCallback(true);
+             }
+             else if (httpRequest.status != 500 || httpRequest.status != 404)
+             {
+                 successCallback(true);
+             }
+             else
+             {
+                 failureCallback(httpRequest.status);
+             }
+         }
+     };
+ 
+     httpRequest.open(requestObject.method, requestObject.URL, true);
+     httpRequest.setRequestHeader('Content-Type', requestObject.contentType);
+ 
+     if (requestObject.method == 'POST')
+     {
+         // If it's a POST method send STRINGIFIED JSON body data.
+         httpRequest.send(requestObject.data);
+     }
+     else
+     {
+         // If it's a GET method then any data should be a query string parameter.
+         httpRequest.send();
+     }
+ }
